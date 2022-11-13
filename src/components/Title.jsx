8bcd15679d7aa39b72286a6359f361fc766be6a5
unknown <mns0327@naver.com>
2022-11-13 21:52:21 +0900

    :sparkles: add font-size option

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 
 const StyledTitle = styled.p`
   color: var(--main-text-color);
-  font-size: 24px;
+  font-size: ${(p) => (p.size ? `${p.size}px` : `${24}px`)};
   margin: 1rem 0;
 `;
 
-const Title = ({children}) => {
-  return <StyledTitle>{children}</StyledTitle>;
+const Title = ({children, size}) => {
+  return <StyledTitle size={size}>{children}</StyledTitle>;
 };
 
 export default Title;
